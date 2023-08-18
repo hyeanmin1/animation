5e547c6f4ae74f5824b7cbea7281ac5e5ad517a5
hyeanmin1 <132598577+hyeanmin1@users.noreply.github.com>
2023-08-19 01:01:21 +0900

    Add files via upload

---
+++ 웹 페이지/web/script.js
@@ -0,0 +1,4 @@
+const element = document.querySelector('.element');
+element.style.position = 'relative'; // 또는 'absolute', 'fixed' 등
+element.style.top = '50px'; // 원하는 위치로 위쪽으로 이동
+element.style.left = '20px'; // 원하는 위치로 왼쪽으로 이동
