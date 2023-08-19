160469578bbac2658f1823dd39a1f1f088409fcf
hyeanmin1 <132598577+hyeanmin1@users.noreply.github.com>
2023-08-19 10:21:33 +0900

    main

--- 웹 페이지/web/script.js
+++ 웹 페이지/web/script.js
@@ -1,4 +1,11 @@
-const element = document.querySelector('.element');
-element.style.position = 'relative'; // 또는 'absolute', 'fixed' 등
-element.style.top = '50px'; // 원하는 위치로 위쪽으로 이동
-element.style.left = '20px'; // 원하는 위치로 왼쪽으로 이동
+const IMG = new Image();
+        IMG.src = 'w91CgcVR0CxvKA7VmxNu4TxyGqGXeWLjjyuGxRFbboluBdfXuCCfL_JXFgEnLtC-c9vMdCPh5ZCE-geGI2uUjmOxR1jimqSnYshtvWjiSinDHHCuOcKrt1hxsaP-7MDKoEaH9BEh4UhBBsj-xqwDOw.webp';
+
+        IMG.addEventListener('load', function() {
+            console.log('높이:', this.naturalHeight, '너비:', this.naturalWidth, '이미지:', this.src);
+
+            const element = document.getElementById('animeImage');
+            element.style.position = 'relative';
+            element.style.top = '50px';
+            element.style.left = '20px';
+        });
